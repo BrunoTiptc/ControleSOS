c22955300369251f4744a093644dd3e576b505fa
Bruno César <121142325+BrunoTiptc@users.noreply.github.com>
2024-04-23 22:11:29 -0300

    Delete SOSentregas directory

--- SOSentregas/versao1.asm
+++
@@ -1,9 +0,0 @@
-0x7fffa5b70b10                  cc                       int3
-0x7fffa5b70b11  <+    1>        c3                       ret
-0x7fffa5b70b12  <+    2>        cc                       int3
-0x7fffa5b70b13  <+    3>        cc                       int3
-0x7fffa5b70b14  <+    4>        cc                       int3
-0x7fffa5b70b15  <+    5>        cc                       int3
-0x7fffa5b70b16  <+    6>        cc                       int3
-0x7fffa5b70b17  <+    7>        cc                       int3
-0x7fffa5b70b18  <+    8>        0f 1f 84 00 00 00 00 00  nopl   0x0(%rax,%rax,1)
